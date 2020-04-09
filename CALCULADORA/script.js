45dc1cc8479f3c6649428b1e0df3d0993866c0c2
Weriton Pires dos Santos <62780016+WeritonPires@users.noreply.github.com>
2020-04-09 00:59:34 -0300

    texte1
    texte não viavel problemas no proeenchimento e muitas linhas de codigo

--- CALCULADORA/script.js
+++ CALCULADORA/script.js
@@ -1,1 +1,8 @@
-alert('ola')
+function num1() {
+    let texto = document.getElementById('txtcal')
+    let n1 = document.getElementById('num1')
+    let numero = Number(n1.value)
+    let item = document.createElement('option')
+    item.text = numero
+    texto.appendChild(item)
+}
